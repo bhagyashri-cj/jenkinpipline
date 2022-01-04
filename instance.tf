f445f4b49f16008644f0ff139d43321e6bfbf773
bhagyashri-cj <70849125+bhagyashri-cj@users.noreply.github.com>
2022-01-04 12:04:10 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,4 +1,4 @@
-provider"aws" {
+provider "aws" {
 
             region = "ap-south-1"
 
@@ -6,21 +6,21 @@ provider"aws" {
 
 
 
-resource"aws_instance""public-web-ec2" {
+resource "aws_instance" "public-web-ec2" {
 
       ami = "ami-0851b76e8b1bce90b"
 
-        instance_type = "t2.micro"
+      instance_type = "t2.micro"
 
-        availability_zone = "ap-south-1a"
+      availability_zone = "ap-south-1a"
 
-        key_name = "docker-key"
+      key_name = "docker-key"
 
-        subnet_id = "subnet-058f5693f1a88c1ba"
+      subnet_id = "subnet-058f5693f1a88c1ba"
 
-        vpc_security_group_ids = ["sg-0001c5a29b3915025"]
+      vpc_security_group_ids = ["sg-0001c5a29b3915025"]
 
-        associate_public_ip_address = true
+      associate_public_ip_address = true
 
 
 
